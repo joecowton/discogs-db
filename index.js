08cd3c6f8555a8843f876bed541a68f82b4cebb6
Joe Cowton <joecowton@gmail.com>
2018-02-11 12:20:59 +0000

    connects dev database

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const mongoose = require('mongoose')
 
 const app = express();
 
+if(process.env.NODE_ENV !== 'test'){
+  mongoose.connect("mongodb://user:user@ds227858.mlab.com:27858/discogs-dev");
+};
+
+
 app.listen(3000, console.log("app listening on 3000"))
