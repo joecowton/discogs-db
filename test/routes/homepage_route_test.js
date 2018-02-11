c16a5d7d4d168acbd447e602ed12f2ce41291b1d
Joe Cowton <joecowton@gmail.com>
2018-02-11 12:28:24 +0000

    test helper and route test setup "

---
+++ test/routes/homepage_route_test.js
@@ -0,0 +1,5 @@
+const assert = require('assert');
+const request = require('supertest');
+const app = require('express');
+const mongoose = require('mongoose');
+const artist = require('../../models/artist');
---
+++ test/test_helper.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+before((done) =>{
+  mongoose.connect('mongodb://user:user@ds229448.mlab.com:29448/disocgs-test')
+  mongoose.connection
+  .once('open', () => { done(); })
+  .on('error', (error) => {
+    console.warn('Warning', error);
+  });
+})
