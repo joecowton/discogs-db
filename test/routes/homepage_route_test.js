f66c634b055d0684ca5e88f45c720033f9ead8b4
Joe Cowton <joecowton@gmail.com>
2018-02-11 12:35:36 +0000

    first test failing

--- test/routes/homepage_route_test.js
+++ test/routes/homepage_route_test.js
@@ -2,4 +2,14 @@ const assert = require('assert');
 const request = require('supertest');
 const app = require('express');
 const mongoose = require('mongoose');
-const artist = require('../../models/artist');
+
+describe("Homepage", () => {
+  it("GET request to '/api' returns welcome message", (done) => {
+    request(app)
+      .get('/api/')
+      .end((err, res) => {
+        assert(res.body.message === 'Welcome to the server')
+        done();
+    })
+  })
+})
