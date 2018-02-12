8ac9e7718c8a115245a58ed098efc3f5b4de246f
Joe Cowton <joecowton@gmail.com>
2018-02-12 12:46:17 +0000

    react app and redux middleware

--- app/services/passport.js
+++ app/services/passport.js
@@ -23,16 +23,14 @@ passport.use(
     callbackURL: '/auth/google/callback',
     proxy: true
   },
-  (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId : profile.id }).then((user) => {
-        if(user){
-          done(null, user);
-        } else{
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      })
+  async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId : profile.id })
+      if(existingUser){
+        done(null, user);
+      } else{
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user);
+      }
     }
   )
 )
