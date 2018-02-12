42bda378f9ae20795fa027dc69c8eb23016c2cf0
Joe Cowton <joecowton@gmail.com>
2018-02-12 09:31:41 +0000

    implement oauth and refactor

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
-const keys = require('./config/keys')
+const bodyParser = require('body-parser');
 const app = express();
-const routes = require('./app/routes/index');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+require('./services/passport')
+require('./routes/authRoutes')(app);
+require('./routes/index')(app);
 
 if(process.env.NODE_ENV !== 'test'){
   mongoose.connect("mongodb://user:user@ds227858.mlab.com:27858/discogs-dev");
@@ -15,23 +15,12 @@ mongoose.connection.on('connected', () => {
   console.log(`Mongoose default connection open to 3000`);
 });
 
-passport.use(
-  new GoogleStrategy({
-    clientID: googleClientID,
-    clientSecret: googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  }, (accessToken) => {
-      console.log(accessToken);
-  })
-)
 
 app.use(bodyParser.json());
 
 app.use((err, req, res, next) => {
   res.status(422).send({ error: err.message });
 });
-
-routes(app);
 
 app.listen(3000, console.log("app listening on 3000"))
 
