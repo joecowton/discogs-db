66f74c3418e30adfffdc27bbc7ab421661da610d
Joe Cowton <joecowton@gmail.com>
2018-02-12 09:51:12 +0000

    saving user google id to database

--- app/app.js
+++ app/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const keys = require('./config/keys')
 const app = express();
 
+require('./models/user_model')
 require('./services/passport')
-require('./routes/authRoutes')(app);
+require('./routes/auth_routes')(app);
 require('./routes/index')(app);
 
 if(process.env.NODE_ENV !== 'test'){
-  mongoose.connect("mongodb://user:user@ds227858.mlab.com:27858/discogs-dev");
+  mongoose.connect(keys.mongoDevURI);
 };
 
 mongoose.connection.on('connected', () => {
@@ -22,6 +24,6 @@ app.use((err, req, res, next) => {
   res.status(422).send({ error: err.message });
 });
 
-app.listen(3000, console.log("app listening on 3000"))
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT)
 module.exports = app;
