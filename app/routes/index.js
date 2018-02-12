0c7f79da33f7b2951236794f72abd7a5d40665f9
Joe Cowton <joecowton@gmail.com>
2018-02-12 11:42:05 +0000

    react app created

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,5 +5,4 @@ module.exports = (app) => {
   app.get('/api/', (req, res) => res.json({ message: "Welcome to the server" }))
   app.post('/api/artists', ArtistController.create)
   app.get('/api/artists', ArtistController.show)
-
 }
