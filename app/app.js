7d0b8e0f5ab0088d1f44039f99a82ff26e803505
Joe Cowton <joecowton@gmail.com>
2018-02-13 10:41:17 +0000

    ready to deploy

--- app/app.js
+++ app/app.js
@@ -5,7 +5,6 @@ const keys = require('./config/keys')
 const cookieSession = require('cookie-session');
 const passport = require('passport')
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 require('./models/user_model')
 require('./services/passport')
@@ -30,6 +29,16 @@ require('./routes/auth_routes')(app);
 require('./routes/billing_routes')(app)
 require('./routes/index')(app);
 
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT)
 
 module.exports = app;
