7b450db23c2b51920a69c62e22d7aad9d3838503
Joe Cowton <joecowton@gmail.com>
2018-02-24 17:09:34 +0000

    working tests for reducers except fetch release

--- client/test/actions/actions.js
+++ client/test/actions/actions.js
@@ -6,12 +6,12 @@ describe('actions', () => {
 	describe('saveComment', () => {
 		it('has the correct type', () => {
 			const action = fetchUser();
-			console.log(action);
+			console.log(action.type);
 			expect(action.type).to.equal(FETCH_USER);
 		});
 
 		it('has the correct payload', () => {
-			const action = saveComment('new comment');
+			const action = fetchUser('new comment');
 			expect(action.payload).to.equal('new comment');
 		});
 	});
---
+++ client/test/reducers/data_reducer_test.js
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import dataReducer from '../../src/reducers/dataReducer';
+import { FETCH_DATA } from '../../src/actions/types';
+import { FETCH_RELEASE } from '../../src/actions/types';
+import { FETCH_ARTIST } from '../../src/actions/types';
+import { SUBMIT_RELEASE } from '../../src/actions/types';
+
+describe('Data Reducer', () => {
+	it('handles action with unkown type', () => {
+		expect(dataReducer(undefined, {})).to.be.instanceOf(Array);
+	});
+
+	it('FETCH_DATA', () => {
+		const action = { type: FETCH_DATA, payload: 'data' };
+		expect(dataReducer([], action)).to.eql(['data']);
+	});
+
+	it('FETCH_RELEASE', () => {
+		const action = { type: FETCH_RELEASE, payload: 'data' };
+		expect(dataReducer([], action)).to.eql(['data']);
+	});
+
+	it('FETCH_ARTIST', () => {
+		const action = { type: FETCH_ARTIST, payload: 'data' };
+		expect(dataReducer([], action)).to.eql('data');
+	});
+
+	it('SUBMIT_RELEASE', () => {
+		const action = { type: SUBMIT_RELEASE, payload: 'data' };
+		expect(dataReducer([], action)).to.eql('data');
+	});
+});
