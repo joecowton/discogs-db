5142e366cb395663313d71bde99575be16820459
Joe Cowton <joecowton@gmail.com>
2018-02-27 21:59:07 +0000

    more tests

--- client/__tests__/actions/actions.test.js
+++ client/__tests__/actions/actions.test.js
@@ -3,22 +3,42 @@ import * as types from '../../src/actions/types';
 
 describe('actions', () => {
 	test('FETCH_USER', () => {
-		expect(actions.FETCH_USER).toEqual('fetch_user');
+		expect(types.FETCH_USER).toEqual('fetch_user');
+	});
+
+	test('fetch_user', () => {
+		expect(typeof actions.fetchUser()).toEqual('function');
 	});
 
 	test('FETCH_DATA', () => {
-		expect(actions.FETCH_DATA).toEqual('fetch_data');
+		expect(types.FETCH_DATA).toEqual('fetch_data');
+	});
+
+	test('fetch_data', () => {
+		expect(typeof actions.fetchData()).toEqual('function');
 	});
 
 	test('FETCH_RELEASE', () => {
-		expect(actions.FETCH_RELEASE).toEqual('fetch_release');
+		expect(types.FETCH_RELEASE).toEqual('fetch_release');
+	});
+
+	test('fetch_release', () => {
+		expect(typeof actions.fetchRelease()).toEqual('function');
 	});
 
 	test('FETCH_ARTIST', () => {
-		expect(actions.FETCH_ARTIST).toEqual('fetch_artist');
+		expect(types.FETCH_ARTIST).toEqual('fetch_artist');
+	});
+
+	test('fetch_artist', () => {
+		expect(typeof actions.fetchArtist()).toEqual('function');
 	});
 
 	test('SUBMIT_RELEASE', () => {
-		expect(actions.SUBMIT_RELEASE).toEqual('submit_release');
+		expect(types.SUBMIT_RELEASE).toEqual('submit_release');
+	});
+
+	test('submitRelease', () => {
+		expect(typeof actions.submitRelease()).toEqual('function');
 	});
 });
